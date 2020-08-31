f04def7e0f17b7184fa229650f21f269c19f687f
s-etty <>
2020-08-30 22:51:32 -0700

    add elevation/speed graph, change to includeCSS() instead of themes = ...

--- running-dashboard/server.R
+++ running-dashboard/server.R
@@ -4,8 +4,9 @@ library(readr)
 library(purrr)
 library(lubridate)
 library(geosphere)
+library(ggplot2)
 
-path <- file.path("running-dashboard/data")
+path <- file.path("data")
 csv_files <- fs::dir_ls(path, regexp = "\\.csv$")
 tracks <- csv_files %>%
     map_dfr(read_csv)
@@ -30,15 +31,50 @@ points <- tracks %>%
     filter(track_id == tracks$track_id[1])
 #select the most recent track as the first points to be drawn
 
+most_recent_date <- unique(date(points$time))
+recorded_dates <- unique(date(tracks$time))
+min_date <- min(recorded_dates)
+n_days <- interval(min_date, most_recent_date)/days(1)
+possible_dates <- min_date + days(0:n_days)
+disabled_dates <- anti_join(x = as_tibble(possible_dates), y = as_tibble(recorded_dates))
+disabled_dates <- pull(disabled_dates, value)
+
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
-    output$date <- renderPrint({input$date})
-    
     output$mymap <- renderLeaflet({
+        points <- tracks %>%
+            filter(date(time) == input$date)
         leaflet() %>%
             addProviderTiles(providers$Stamen.TonerLite,
                              options = providerTileOptions(noWrap = TRUE)
             ) %>%
             addPolylines(lng = points$lon, lat = points$lat)
+    })
+    
+    output$elevation <- renderPlotly({
+        points <- tracks %>%
+            filter(date(time) == input$date)
+        
+        plot <- plot_ly(
+            x = points$time,
+            y = points$elevation
+        ) %>%
+            add_lines(name = "Elevation") %>%
+            add_lines(name = "Speed", y = points$speed, color = "red") %>%
+            layout(paper_bgcolor = "#2B2E32", plot_bgcolor = "#2B2E32",
+                   xaxis = list(
+                       color = "white",
+                       showgrid = FALSE
+                   ),
+                   yaxis = list(
+                       color = "white",
+                       title = "Elevation (m)",
+                       showgrid = FALSE
+                   ),
+                   legend = list(
+                       font = list(
+                           color = "white"
+                       )
+                   ))
     })
 })
--- running-dashboard/ui.R
+++ running-dashboard/ui.R
@@ -1,8 +1,9 @@
 library(shiny)
 library(leaflet)
+library(plotly)
 
 shinyUI(fluidPage(
-    theme = "styles.css",
+    includeCSS("styles.css"),
     fluidRow(class = "header-navbar",
         column(4,
                p("sidebar expand button")),
@@ -11,11 +12,20 @@ shinyUI(fluidPage(
         column(4, id = "user-profile-header",
                p("user profile button"))
     ),
-    fluidRow(class = "latest-track",
+    fluidRow(class = "track-map",
              column(2, 
-                    dateInput("date", label = h3("Select a Date"), value = today())),
+                    dateInput("date", label = h5("Select a Date"), 
+                              value = "2020-08-27")),
+                              #value = most_recent_date,
+                              #min = min_date, max = most_recent_date,
+                              #datesdisabled = disabled_dates$value)),
+             #need to figure out how to disable all dates except those specified
              column(10, 
                     leafletOutput("mymap"))
                     
+    ),
+    fluidRow(class = "track-stats",
+             column(8,
+                    plotlyOutput("elevation"))
     )
 ))
