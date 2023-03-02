1b6e32ad43db6297b26281dee3d12d337cb98dee
s-etty <5265145+s-etty@users.noreply.github.com>
2023-03-01 17:35:48 -0800

    update comments, remove data from gitignore, add data folder

--- running-dashboard/server.R
+++ running-dashboard/server.R
@@ -14,86 +14,86 @@ imperial_metric <- function(measurement, units) {
     imperial_measurement <- case_when(
         #convert to mph
         units == "m/s" ~ measurement * 2.3694,
-        #convert to ft, useful for elevation
+        # convert to ft, useful for elevation
         units == "m" ~ measurement * 3.28084,
-        #convert to miles
+        # convert to miles
         units == "km" ~ measurement * 0.621371,
-        #convert from meters to miles
+        # convert from meters to miles
         units == "meters" ~ measurement * 0.000621371
     )
 }
 
-#read the data file path and create a vector of the csv filenames
+# read the data file path and create a vector of the csv filenames
 path <- file.path("data")
 csv_files <- fs::dir_ls(path, regexp = "\\.csv$")
 
-#create a fs_path object of all the csv files in the above directory (data/...)
+# create a fs_path object of all the csv files in the above directory (data/...)
 tracks <- csv_files %>%
-    #map the read_csv function to the fs_path object "csv_files", i.e., read all the csv's
+    # map the read_csv function to the fs_path object "csv_files", i.e., read all the csv's
     map_dfr(read_csv) %>%
-    #rename the time column to track_timestamp to avoid function name conflict
+    # rename the time column to track_timestamp to avoid function name conflict
     rename(track_timestamp = time) %>%
-    #use the first tracks lat and lon to define the timezone.
-    #there could be an opportunity to set the timezone for each track, but that will come later
-    #also the reason for this is because if no timezone is specified it will use the server's
-    #timezone when deployed to a shiny server and can cause errors
+    # use the first tracks lat and lon to define the timezone.
+    # there could be an opportunity to set the timezone for each track, but that will come later
+    # also the reason for this is because if no timezone is specified it will use the server's
+    # timezone when deployed to a shiny server and can cause errors
     mutate(track_timestamp = with_tz(track_timestamp, 
                                      tzone = tz_lookup_coords(lat[1], lon[1]))) %>%
-    #change the elevation to ft
+    # change the elevation to ft
     mutate(elevation = imperial_metric(elevation, "m")) %>%
-    #change the speed to mph
+    # change the speed to mph
     mutate(speed = imperial_metric(speed, "m/s"))
 
-#group the tracks by date, then generate an id for each track
+# group the tracks by date, then generate an id for each track
 track_id_df <- tracks %>%
+    # extract the date from the timestamps
     mutate(track_date = date(track_timestamp)) %>%
-    #extract the date from the timestamps
+    # group them by track dates, will not work for days with multiple workouts
     group_by(track_date) %>%
-    #group them by track dates, will not work for days with multiple workouts
+    # assign an ID to each group
     group_indices(track_date)
-    #assign an ID to each group
 
-#prepare the tracks df for display
+# prepare the tracks df for display
 tracks <- tracks %>%
-    #add a track_id column, may go away when connect to a database
+    # add a track_id column, may go away when connect to a database
     mutate(track_id = cbind(track_id_df)) %>%
-    #sort descending date, i.e., newest at the top
+    # sort descending date, i.e., newest at the top
     arrange(desc(track_timestamp)) %>%
-    #drop any duplicate rows
+    # drop any duplicate rows
     distinct(track_timestamp, .keep_all = TRUE) %>%
-    #group the tracks together
+    # group the tracks together
     group_by(track_id) %>%
-    #calculate elapsed time
+    # calculate elapsed time
     mutate(runtime_min = difftime(first(track_timestamp), track_timestamp, units = "mins")) %>%
-    #calculate elapsed time in seconds, easier for some calculations
+    # calculate elapsed time in seconds, easier for some calculations
     mutate(runtime_sec = difftime(first(track_timestamp), track_timestamp, units = "secs")) %>%
-    #create columns for the next lat and lon to calculate distance between points
+    # create columns for the next lat and lon to calculate distance between points
     mutate(nextLat = lead(lat), nextLon = lead(lon)) %>%
-    #calculate distances between points
+    # calculate distances between points
     rowwise() %>%
     mutate(distance = distVincentyEllipsoid(c(lon, lat), c(nextLon, nextLat))) %>%
     ungroup() %>%
     # convert to imperial
     mutate(distance = imperial_metric(distance, "meters"))
 
-#get the most recent recording
+# get the most recent recording
 most_recent_date <- date(tracks$track_timestamp[1])
-#get the unique recorded dates
+# get the unique recorded dates
 recorded_dates <- unique(date(tracks$track_timestamp))
-#get the min recorded date
+# get the min recorded date
 min_date <- min(recorded_dates)
-#count the number of days between oldest and most recent recording
+# count the number of days between oldest and most recent recording
 n_days <- interval(min_date, most_recent_date)/days(1)
 #dates between the min and most recent
 possible_dates <- min_date + days(0:n_days)
-#anti-join the possible dates with dates actually recorded. This will be used to gray out
+# anti-join the possible dates with dates actually recorded. This will be used to gray out
 # selections on the calendar picker.
 disabled_dates <- anti_join(x = as_tibble(possible_dates), y = as_tibble(recorded_dates)) %>%
     pull(value)
 
 
 shinyServer(function(input, output) {
-    #create a date selector to filter which tracks to show
+    # create a date selector to filter which tracks to show
     output$date_input <- renderUI({
         dateInput("date", label = h5("Select a Date"),
                   value = most_recent_date,
@@ -101,28 +101,28 @@ shinyServer(function(input, output) {
                   datesdisabled = disabled_dates)
     })
     
-    #create a map to plot the tracks onto
+    # create a map to plot the tracks onto
     output$mymap <- renderLeaflet({
-        #because JS is asynchronous, this will load before some of the above code finishes
-        #flasing a short error message if you don't include the if statement. kind of hackey
+        # because JS is asynchronous, this will load before some of the above code finishes
+        # flasing a short error message if you don't include the if statement. kind of hackey
         if(!is.null(input$date)){
-            #filter using the date selected from the dateInput above
+            # filter using the date selected from the dateInput above
             points <- tracks %>%
                 filter(date(track_timestamp) == input$date)
-
+            
             leaflet() %>%
-                #tells which map tiles to use
+                # tells which map tiles to use
                 addProviderTiles(providers$Stamen.TonerLite) %>%
-                #plot each lat and lon as a point in the polygon
+                # plot each lat and lon as a point in the polygon
                 addPolylines(lng = points$lon, lat = points$lat,
                              col = "#CD5C5C")
         }
     })
     
-    #create the elevation and speed plots
+    # create the elevation and speed plots
     output$elevation_speed <- renderPlotly({
-        #because JS is asynchronous, this will load before some of the above code finishes
-        #flasing a short error message if you don't include the if statement. kind of hackey
+        # because JS is asynchronous, this will load before some of the above code finishes
+        # flasing a short error message if you don't include the if statement. kind of hackey
         if(!is.null(input$date)){
             points <- tracks %>%
                 filter(date(track_timestamp) == input$date) %>%
@@ -130,7 +130,9 @@ shinyServer(function(input, output) {
                 mutate(track_timestamp_30s = round_date(track_timestamp, unit = "30s")) %>%
                 # group by 30s intervals
                 group_by(track_timestamp_30s) %>%
-                # get the mean elevation and speed for 30s intervals. This will hopefully smooth out the plots while still maintaining some of the resolution
+                # get the mean elevation and speed for 30s intervals. This will
+                # hopefully smooth out the plots while still maintaining some of
+                # the resolution
                 mutate(elevation_30s = mean(elevation),
                        speed_30s = mean(speed)) %>%
                 ungroup()
@@ -183,7 +185,8 @@ shinyServer(function(input, output) {
                        )
                 )
             
-            # put both the elevation and speed plot on the same subplot object and have them share an x axis
+            # put both the elevation and speed plot on the same subplot object
+            # and have them share an x axis
             elevation_speed <- subplot(elevation_plot, speed_plot,
                                        nrows = 2, shareX = TRUE,
                                        titleY = TRUE)
@@ -231,7 +234,7 @@ shinyServer(function(input, output) {
                 div(class = "col-sm-6",
                     p(class = "stats",
                       sprintf("%02d:%02d", minute(summary_stats$total_runtime),
-                                               second(summary_stats$total_runtime)),
+                              second(summary_stats$total_runtime)),
                       icon("stopwatch", lib = "font-awesome")),
                     p(class = "stats-descriptor", "Runtime"),
                     br(),
@@ -246,7 +249,7 @@ shinyServer(function(input, output) {
                     p(class = "stats-descriptor", "Vertical Gain ft")
                 )
             )
-            #the vertical gain is not working
+            # the vertical gain is not working
         }
     })
 })
--- running-dashboard/ui.R
+++ running-dashboard/ui.R
@@ -10,16 +10,35 @@ library(leaflet)
 library(plotly)
 
 shinyUI(fluidPage(
+    # include a style sheet for custom colors and styling
     includeCSS("styles.css"),
-    #
+    
+    # the code below is not complete and represents future upgrades to the
+    # dashboard. It is under construction.
+    ############################################
+    # add a row for the header navbar. this will include user buttons, settings, and info links
+    # fluidRow(class = "header-navbar",
+    #          column(4,
+    #                 p("sidebar expand button (coming soon)")),
+    #          column(4, id = "page-title",
+    #                 h1(class = "header-navbar", "Run Dash")),
+    #          column(4, id = "user-profile-header",
+    #                 p("user profile button (coming soon)"))
+    # ),
+    ############################################
+    
+    # add a row for the title of the page. will be upgraded to a navbar later on.
+    ############################################
     fluidRow(class = "header-navbar",
-             column(4,
-                    p("sidebar expand button (coming soon)")),
-             column(4, id = "page-title",
-                    h1(class = "header-navbar", "Run Dash")),
-             column(4, id = "user-profile-header",
-                    p("user profile button (coming soon)"))
-    ),
+             column(12, id = "page-title",
+                    h1(class = "header-navbar", "Run Dash"))),
+    ############################################
+    
+    # add a row for the calendar and map. these items link to the date_input and mymap
+    # objects from the server.R file
+    # the calender takes up 2/12 parts of the screen
+    # the map takes up 10/12 parts of the screen
+    ############################################
     fluidRow(class = "track-map",
              column(2, 
                     uiOutput("date_input")),
@@ -27,17 +46,31 @@ shinyUI(fluidPage(
                     leafletOutput("mymap"))
              
     ),
+    ############################################
+    
+    # add a horizontal rule aka a line
     hr(),
+    
+    # add a row for the stats and elevation and speed plots
+    # these items link to the current_summary_stats and elevation_speed outputs from the server.R
+    # file.
+    # stats takes up 4/12 parts of the screen
+    # elevation and speed charts take up 8/12 parts of the screen
+    ############################################
     fluidRow(class = "track-stats",
              column(4,
                     htmlOutput("current_summary_stats")),
              column(8,
                     plotlyOutput("elevation_speed"))
 
-    ),
-    fluidRow(class = 'testing',
-             column(12,
-                    htmlOutput("timezone_test")))
+    )
+    ############################################
+    
+    # the code below is not complete and represents future upgrades to the
+    # dashboard. It is under construction.
+    ############################################
+    # below is code to add lifetime stats, e.g., number of miles run in the last week, month, year, 
+    # lifetime.
     # fluidRow(class = "lifetime-stats",
     #          column(1),
     #          column(7,
@@ -55,4 +88,5 @@ shinyUI(fluidPage(
     #                         selected = "Week")
     #              )
     # )
+    ############################################
 ))
